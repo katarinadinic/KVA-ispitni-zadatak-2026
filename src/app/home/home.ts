2b2472f0e1fb3c89640dc93a720859ff58954315
katarinadinic <katarinadinic.0909@gmail.com>
2026-04-20 22:12:16 +0200

    Initial commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import axios from 'axios';
+import { ToyModel } from '../../models/toy.model';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  toys = signal<ToyModel[]>([])
+
+  constructor() {
+    axios.get('https://toy.pequla.com/api/toy')
+      .then(rsp => this.toys.set(rsp.data))
+  }
+}
