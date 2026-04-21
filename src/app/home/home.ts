4db2830ed3e68548e2b70a20632a41fa97e66b5f
katarinadinic <katarinadinic.0909@gmail.com>
2026-04-21 21:57:38 +0200

    Add angular material

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import axios from 'axios';
 import { ToyModel } from '../../models/toy.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
